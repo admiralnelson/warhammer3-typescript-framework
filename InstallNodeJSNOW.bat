02fcba4713cc60a74e8534b98bb5438153f47024
admiralnelson <admiralofinternetmeme@outlook.com>
2023-01-24 13:41:55 +0700

    install git too

--- InstallNodeJSNOW.bat
+++ InstallNodeJSNOW.bat
@@ -1,8 +1,14 @@
 @echo off
 where node
 if %ERRORLEVEL% == 1 (
-    winget install nodejs
+    winget install OpenJS.NodeJS
 ) else (
     echo you have nodejs already
+)
+where git
+if %ERRORLEVEL% == 1 (
+    winget install Git.git
+) else (
+    echo you have git already
 )
 pause
